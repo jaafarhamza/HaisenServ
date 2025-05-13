9a9a609b099d1e735e815ec6537f4ff60a4b2857
jaafarhamza <jaafar.hamza711@gmail.com>
2025-05-13 06:02:59 +0100

    Add custom exception handling for better error management

---
+++ app/Exceptions/Custom/BookingException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exceptions\Custom;
+
+use Exception;
+
+class BookingException extends Exception
+{
+    // Custom exception for booking-related errors
+}
---
+++ app/Exceptions/Custom/UnauthorizedActionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exceptions\Custom;
+
+use Exception;
+
+class UnauthorizedActionException extends Exception
+{
+    // Custom exception for unauthorized actions
+}
